7b2a96716aab8b9814cfbffd386aff6180f73337
A-yong <2378786248@qq.com>
2024-06-08 22:40:05 +0800

    project init

---
+++ src/test/java/com/doupi/springbootinit/SpringbootInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doupi.springbootinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
